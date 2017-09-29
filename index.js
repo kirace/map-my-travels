d257b393e429b11fa606c849d79b08edb16719a8
Kevin Irace <kevinirace@Kevins-MacBook-Pro.local>
2017-09-28 22:00:47 -0400

    city update

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var pg = require('pg');
+var MapboxClient = require('mapbox');
+var passwordHash = require('password-hash');
+var cookieParser = require('cookie-parser');
 
 const config = {
   user: 'ajoxkpklisbuqh',
@@ -14,15 +17,20 @@ const config = {
 };
 
 var pool = new pg.Pool(config);
-
 var bodyParser = require('body-parser');
+var token = 'pk.eyJ1Ijoia2V2aW5pcmFjZSIsImEiOiJjajY0M2ExZDIxbm1hMzNwOHp0cWpzbjJkIn0.g_KHQKSe60ViArp6hW-2TA';
+var client = new MapboxClient(token);
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-fo
+app.use(cookieParser("K secret"));
 
 app.use('/node_modules',  express.static(__dirname + '/node_modules'));
 
 app.use('/style',  express.static(__dirname + '/style'));
+
+app.use('/images',  express.static(__dirname + '/images'));
+
 
 app.get('/',function(req,res){
     res.sendFile('home.html',{'root': __dirname + '/public'});
@@ -48,47 +56,65 @@ app.get('/account',function(req,res){
     res.sendFile('account.html',{'root': __dirname + '/public'});
 });
 
+app.get('/logOut', function(req, res) {
+  res.clearCookie('myUsername');
+  console.log('Logging Out');
+});
+
+
 app.get('/getData', function(request, response) {
-  var email = request.query.email;
+
+  var email = request.signedCookies['myUsername'];
   console.log('email: ', email);
   response.contentType('application/json');
+  var res = {};
+  var countries = [], cities = [];
 
   pool.connect(function(err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-
-  client.query('SELECT countries FROM user_data WHERE username = $1 ;', [email], function(err, result) {
-    //call 'done()' to release the client back to the pool
-    done();
-
     if(err) {
-      return console.error('error running query', err);
+      return console.error('error fetching client from pool', err);
     }
-    console.log(result);
-    console.log(result.rows[0].countries);
-    response.json(result.rows[0].countries);
 
-    //output: 1
+    client.query('SELECT country FROM user_countries WHERE username = $1 ;', [email], function(err, result) {
+
+      if(err) {
+        return console.error('error running query', err);
+      }
+      for(i = 0; i < result.rows.length; i++){
+        countries.push(result.rows[i].country);
+      }
+      console.log(countries);
+      res.countryList = countries;
+
+      client.query('SELECT city FROM user_cities WHERE username = $1 ;', [email], function(err, result) {
+        done();
+
+        if(err) {
+          return console.error('error running query', err);
+        }
+
+        for(i = 0; i < result.rows.length; i++){
+          cities.push(result.rows[i].city);
+        }
+        console.log(cities);
+        res.cityList = cities;
+        console.log(res);
+        response.json(res);
+      });
+    });
   });
-});
 
 });
 
 app.get('/getCountries', function(request, response) {
   response.contentType('application/json');
-
   results = [];
-
   pool.connect(function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
   }
-
   client.query('SELECT * FROM countries;', function(err, result) {
-    //call 'done()' to release the client back to the pool
     done();
-
     if(err) {
       return console.error('error running query', err);
     }
@@ -97,23 +123,33 @@ app.get('/getCountries', function(request, response) {
       results.push(temp);
     }
     response.json(results);
-
+  });
   });
 });
 
+app.get('/addCity', function(request, response) {
+  response.contentType('application/json');
+  var query = String(request.query.city);
+  var result;
+  client.geocodeForward(query, {
+    limit: 1,
+    types: 'place'
+  }, function(err, data, res) {
+    result = data;
+    console.log('ADDED CITY: ', result);
+    response.json(result);
+  });
 });
 
 app.post('/register', function(req, res) {
-
   pool.connect(function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
   }
+  var hashedPassword = passwordHash.generate(req.body.inputPassword);
 
-  client.query('INSERT INTO user_data VALUES ($1, $2, DEFAULT, DEFAULT);',[req.body.inputEmail, req.body.inputPassword], function(err, result) {
-    //call 'done()' to release the client back to the pool
+  client.query('INSERT INTO users VALUES ($1, $2, DEFAULT, DEFAULT);',[req.body.inputEmail, hashedPassword], function(err, result) {
     done();
-
     if(err) {
       return console.error('error running query', err);
       res.redirect('/showSignUpPage');
@@ -121,7 +157,6 @@ app.post('/register', function(req, res) {
     }
     res.redirect('/registered');
     res.end();
-    //output: 1
   });
 
 });
@@ -130,53 +165,96 @@ app.post('/register', function(req, res) {
 app.post('/verifyuser', function(req,res){
 
   pool.connect(function(err, client, done) {
+
   if(err) {
     return console.error('error fetching client from pool', err);
   }
 
-  client.query('SELECT password FROM user_data WHERE username = $1 ;', [req.body.inputEmail], function(err, result) {
-    //call 'done()' to release the client back to the pool
+  client.query('SELECT password FROM users WHERE username = $1 ;', [req.body.inputEmail], function(err, result) {
+
     done();
 
     if(err) {
       return console.error('error running query', err);
     }
 
-    if(result.rows[0].password == req.body.inputPassword){
-      var string = req.body.inputEmail;
-      res.redirect('/account?valid='+string);
-      res.end();
-    }
-    else{
-      console.log("user not found");
+    if(result.rows.length < 1){
       res.redirect('/showSignInPageretry');
       res.end();
     }
-
+    else{
+      let hashPW = result.rows[0].password;
+      if(passwordHash.verify(req.body.inputPassword, hashPW)){
+        let options = {
+          httpOnly: true, // The cookie only accessible by the web server
+          signed: true // Indicates if the cookie should be signed
+        }
+        res.cookie('myUsername', req.body.inputEmail, options);
+        res.redirect('/account');
+        res.end();
+      }
+      else{
+        console.log("user not found");
+        res.redirect('/showSignInPageretry');
+        res.end();
+      }
+    }
+  });
   });
 });
-});
 
-app.post('/saveData', function(req,res){
-  var user_name = req.body.user;
-  var user_countries = JSON.stringify(req.body.countries);
+app.post('/updateCountryDB', function(req,res){
 
-  user_countries = user_countries.replace('[','{').replace(']','}');
+  var action = req.body.action;
+  var country = req.body.country;
+  var user_name = req.signedCookies['myUsername'];
+  var query = '';
+
+  if(action === 'ADD'){
+    query = 'INSERT INTO user_countries VALUES ($1, $2);'
+  }
+  if(action === 'REMOVE'){
+    query = 'DELETE FROM user_countries WHERE username = $1 AND country = $2;';
+  }
 
   pool.connect(function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
   }
-
-  client.query('UPDATE user_data SET countries = $1 WHERE username = $2 ;', [user_countries, user_name], function(err, result) {
-    //call 'done()' to release the client back to the pool
+  client.query(query, [user_name, country], function(err, result) {
     done();
-
     if(err) {
       return console.error('error running query', err);
     }
   });
+  });
 });
+
+app.post('/updateCityDB', function(req,res){
+
+  var action = req.body.action;
+  var city = req.body.city;
+  var user_name = req.signedCookies['myUsername'];
+  var query2 = '';
+
+  if(action === 'ADD'){
+    query2 = 'INSERT INTO user_cities VALUES ($1, $2) ON CONFLICT DO NOTHING;'
+  }
+  if(action === 'REMOVE'){
+    query2 = 'DELETE FROM user_cities WHERE username = $1 AND city = $2;';
+  }
+
+  pool.connect(function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query(query2, [user_name, city], function(err, result) {
+    done();
+    if(err) {
+      return console.error('error running query', err);
+    }
+  });
+  });
 });
 
 http.listen(process.env.PORT || 3000, function(){
